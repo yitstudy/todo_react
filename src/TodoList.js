b32cd7b4e8a02790e458365225e99bdef93dbe36
yitstudy <yuito22suto@gmail.com>
2025-04-13 15:12:53 +0900

    setup

---
+++ src/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TodoList({ todos }) {
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <li key={index}>{todo}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
